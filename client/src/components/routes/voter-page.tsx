db91ad364650635637ceac19a49c707ebdbe4c5d
razbensimon <raz.bensimon@env0.com>
2022-07-30 13:54:57 +0300

    order

--- client/src/components/routes/voter-page.tsx
+++ client/src/components/routes/voter-page.tsx
@@ -3,6 +3,7 @@ import ContractInfoSection from '../admin/contract-info-section';
 import React from 'react';
 import { Divider } from 'antd';
 import AddVoterForm from '../voter/vote-form';
+import { ElectionsResults } from '../results';
 
 export default function VoterPage() {
   return (
@@ -10,11 +11,11 @@ export default function VoterPage() {
       <ContractInfoSection />
       <Welcome />
       <Divider />
-      ElectionResults:
-      <Divider />
       <AddVoterForm />
       <Divider />
       VoteHelper:
+      <Divider />
+      <ElectionsResults />
     </div>
   );
 }
