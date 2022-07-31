3082fc2be11d3b08986b3afdbfa5d65f80a0c608
razbensimon <raz.bensimon@env0.com>
2022-07-31 20:04:04 +0300

    fox permissions

--- truffle/contracts/RazCoin.sol
+++ truffle/contracts/RazCoin.sol
@@ -8,7 +8,7 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 contract RazCoin is Ownable, ERC20 {
     constructor() Ownable() ERC20('Raz Coin', 'RAZ') {}
 
-    function mint(address _to, uint256 _amount) public {
+    function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount * 10**decimals());
     }
 }
