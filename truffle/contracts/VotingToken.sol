4195c33bbcfca6e54cfa475502b1d968eadbf43e
razbensimon <raz.bensimon@env0.com>
2022-07-31 19:14:11 +0300

    note 'SafeMath' is not needed starting with Solidity 0.8 :) https://ethereum.stackexchange.com/questions/91367/is-the-safemath-library-obsolete-in-solidity-0-8-0

--- truffle/contracts/VotingToken.sol
+++ truffle/contracts/VotingToken.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+// NOTE: 'SafeMath' is not needed starting with Solidity 0.8 :)
+
 contract VotingToken is Ownable, ERC721 {
 	uint256 public tokenCounter;
 
