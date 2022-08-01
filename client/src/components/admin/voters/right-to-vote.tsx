7d577cfae58dd644b6eba104ec11c43a041cd374
razbensimon <raz.bensimon@env0.com>
2022-08-01 20:11:33 +0300

    right-to-vote show import address

--- client/src/components/admin/voters/right-to-vote.tsx
+++ client/src/components/admin/voters/right-to-vote.tsx
@@ -10,25 +10,35 @@ const RightToVote: React.FC<Props> = () => {
 
   const { status } = useElectionsStatus();
   const [rightToVote, setRightToVote] = useState<boolean>();
+  const [importAddress, setImportAddress] = useState<string>();
 
   useEffect(() => {
     if (!contract) return;
     (async () => {
       const hasRight = await contract.methods.hasVotingRight().call({ from: accounts[0] });
+      const contractAddress = await contract.methods.rightToVote().call({ from: accounts[0] });
+      console.log('rightToVote contract address:', contractAddress);
+
       setRightToVote(hasRight);
+      setImportAddress(contractAddress);
     })();
   }, [contract, accounts]);
 
   return (
     <div>
       {rightToVote ? (
-        <div style={{ color: 'green' }}>You have the right to vote in those elections!</div>
+        <div>
+          <div style={{ color: 'green' }}>- You have the right to vote in those elections!</div>
+          <span style={{ paddingLeft: '1em' }}>
+            Import ERC721 token in metamask: <code style={{ display: 'inline-block' }}>{importAddress}</code>
+          </span>
+        </div>
       ) : (
         <div style={{ color: 'red' }}>You do NOT have the right to vote on those elections!</div>
       )}
 
       <div style={{ paddingTop: '1em' }}>
-        Are the elections open for voting? - {status === Status.Voting ? 'Yes' : 'No'}
+        - Are the elections open for voting? - {status === Status.Voting ? 'Yes' : 'No'}
       </div>
     </div>
   );
